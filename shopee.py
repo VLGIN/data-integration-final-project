8fa805c7bab818e65da916c09bad83fcffae85e4
dungpv <dung.pv230100@gmail.com>
2022-06-15 14:30:15 +0700

    add sendo

--- shopee.py
+++ shopee.py
@@ -8,7 +8,6 @@ import ast
 
 
 def call_api(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     r = requests.get(url).json()
     time.sleep(2)
     return r
