df07b6bc91c68f6c1361889af085dab187b959db
tdd75 <tranducduy7520@gmail.com>
2022-07-04 20:03:29 +0700

    feat: add chart

--- web-demo/.eslintrc.js
+++ web-demo/.eslintrc.js
@@ -10,8 +10,9 @@ module.exports = {
   },
   rules: {
     'no-unused-vars': 'off',
-    'unused-imports/no-unused-imports': 'warn',
-    'unused-imports/no-unused-vars': 'warn',
+    'no-undef': 'off',
+    'unused-imports/no-unused-imports': 'off',
+    'unused-imports/no-unused-vars': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
